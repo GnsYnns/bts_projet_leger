aa6088db7f676fb6748cd952e63564313459d0cb
GnsYnns <135223509+GnsYnns@users.noreply.github.com>
2023-09-25 15:49:55 +0200

    Update generic.blade.php

--- project_leger/resources/views/templates/generic.blade.php
+++ project_leger/resources/views/templates/generic.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo asset('css/app.css')?>" type="text/css">
         <title>rB&B</title>
         
     </head>
